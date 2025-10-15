f4ad5283949148e99dda7fad265147301aaab372
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-10-15 16:08:36 +0200

    wip: fix oopsies
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- boards/mercedes/ardep/sysbuild.cmake
+++ boards/mercedes/ardep/sysbuild.cmake
@@ -2,12 +2,14 @@ if(SB_CONFIG_BOOT_LOGIC_UDS_FIRMWARE_LOADER)
   ExternalZephyrProject_Add(
     APPLICATION firmware-loader
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../../../firmware-loader
+    APP_TYPE FIRMWARE_LOADER
   )
 
   set_config_bool(mcuboot CONFIG_BOOT_FIRMWARE_LOADER_ENTRANCE_GPIO y)
   set_config_int(mcuboot CONFIG_BOOT_FIRMWARE_LOADER_DETECT_DELAY 0)
   set_config_bool(mcuboot CONFIG_BOOT_FIRMWARE_LOADER_BOOT_MODE y)
   set_config_bool(mcuboot CONFIG_BOOT_FIRMWARE_LOADER_NO_APPLICATION y)
+  set(mcuboot_EXTRA_DTC_OVERLAY_FILE "${CMAKE_CURRENT_LIST_DIR}/mcuboot.overlay" CACHE STRING "")
 endif()
 
 if(SB_CONFIG_BOOT_LOGIC_LEGACY)
