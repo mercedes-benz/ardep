988ff49e89e1bb0f75a51fc0f2d53a82d9888609
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-11-04 14:41:10 +0100

    wip: license
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- boards/common/ardep.board.cmake
+++ boards/common/ardep.board.cmake
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (C) 2025 Frickly Systems GmbH
+# SPDX-FileCopyrightText: Copyright (C) 2025 MBition GmbH
+#
 # SPDX-License-Identifier: Apache-2.0
 
 board_set_flasher_ifnset(ardep)
