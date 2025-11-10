12f938b4c79b4c6b685bb1ec358101830bf99820
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-11-10 09:47:00 +0100

    wip: add sample
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

---
+++ samples/uds_bus_sample/src/uds.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (C) Frickly Systems GmbH
+ * SPDX-FileCopyrightText: Copyright (C) MBition GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ARDEP_SAMPLES_UDS_SRC_UDS_H
+#define ARDEP_SAMPLES_UDS_SRC_UDS_H
+
+#include <ardep/uds.h>
+
+extern struct uds_instance_t instance;
+
+#endif  // ARDEP_SAMPLES_UDS_SRC_UDS_H
