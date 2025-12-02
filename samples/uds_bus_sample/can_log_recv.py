5bb0f024e4a7a99b7b06c91812a662ed72bf2c3a
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-12-02 13:03:24 +0100

    wip: small improvements
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- samples/uds_bus_sample/can_log_recv.py
+++ samples/uds_bus_sample/can_log_recv.py
@@ -7,7 +7,6 @@
 import can
 from argparse import ArgumentParser
 
-
 def main(args):
     interface = args.interface
     id = args.id
@@ -24,7 +23,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="CAN Log Receiver")
-    parser.add_argument("-i","--interface", type=str, default="can0", help="CAN interface to use (default: can0)")
-    parser.add_argument("-id","--id", type=lambda x: int(x, 0), default=0x100, help="CAN ID to listen to (default: 0x100)")
+    parser.add_argument("-i", "--interface", type=str, default="can0", help="CAN interface to use (default: can0)")
+    parser.add_argument("-id", "--id", type=lambda x: int(x, 0), default=0x100, help="CAN ID to listen to (default: 0x100)")
     args = parser.parse_args()
     main(args)
