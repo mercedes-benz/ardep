38523638fe2791f44b90468d237c110ab455e47d
Mark Oude Elberink <mark.oude.elberink@frickly.systems>
2025-12-02 16:03:54 +0100

    wip
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- samples/power_io_shield/end_of_line_tester/src/main.c
+++ samples/power_io_shield/end_of_line_tester/src/main.c
@@ -27,9 +27,49 @@ DEFINE_GPIO_ARRAY(DT_PATH(zephyr_user), output_gpios);
 DEFINE_GPIO_ARRAY(DT_PATH(zephyr_user), input_gpios);
 DEFINE_GPIO_ARRAY(DT_PATH(zephyr_user), fault_gpios);
 
+BUILD_ASSERT(ARRAY_SIZE(output_gpios) == ARRAY_SIZE(input_gpios),
+             "Number of output and input GPIOs must match");
+
+int assert_input_pins(const bool* expected_states) {
+  for (size_t i = 0; i < ARRAY_SIZE(input_gpios); i++) {
+    int val = gpio_pin_get(input_gpios[i].port, input_gpios[i].pin);
+    if (val < 0) {
+      LOG_ERR("Failed to read input GPIO %d: %d", i, val);
+      return val;
+    }
+
+    bool expected = expected_states[i];
+    if (val != (int)expected) {
+      LOG_ERR("Input GPIO %d state %d does not match expected %d", i, val,
+              expected);
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
 int main() {
   if (!device_is_ready(power_io_shield)) {
     LOG_ERR("HV Shield device not ready");
     return 1;
+  }
+
+  bool expected_input_states[ARRAY_SIZE(input_gpios)] = {0};
+
+  LOG_INF("Starting End of line tester in 1s");
+  k_sleep(K_SECONDS(1));
+  // setup all pins as low
+  for (size_t i = 0; i < ARRAY_SIZE(output_gpios); i++) {
+    int err = gpio_pin_configure_dt(&output_gpios[i], GPIO_OUTPUT_INACTIVE);
+    if (err) {
+      LOG_ERR("Failed to configure output GPIO %d: %d", i, err);
+      return err;
+    }
+  }
+  int err = assert_input_pins(expected_input_states);
+  if (err) {
+    LOG_ERR("Initial input pin state check failed");
+    return err;
   }
 }
